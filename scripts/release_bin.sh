32139e4a9ed81d34533e1446b230f1a527618c71
vitali2y <vitaliyy@gmail.com>
2018-03-09 13:48:49 +0200

    OSX support

--- scripts/release_bin.sh
+++ scripts/release_bin.sh
@@ -15,9 +15,11 @@ if [ $? -ne 0 ]; then echo 'failed!!!'; cd -; exit 1; fi
 ../node_modules/.bin/coffee -o . -c ../server/*.coffee &&
 TARGETS="node8-linux-x64" && ../node_modules/.bin/pkg --targets $TARGETS --output ./server-linux-x64.bin ./package.json &&
 mv ./server-linux-x64.bin ../dist/linux &&
+TARGETS="node8-macos-x64" && ../node_modules/.bin/pkg --targets $TARGETS --output ./server-macos-x64.bin ./package.json &&
+mv ./server-macos-x64.bin ../dist/darwin &&
 TARGETS="node8-win-x86" && ../node_modules/.bin/pkg --targets $TARGETS --output ./server-win-x86.exe ./package.json &&
 mv ./server-win-x86.exe ../dist/windows &&
 cd -)
 
 GREEN='\033[0;32m'; NOCOLOR='\033[0m'
-echo "start server (Linux ${GREEN}server-linux-x64.bin${NOCOLOR} or, Winduz ${GREEN}server-win-x86.exe${NOCOLOR})"
+echo "start server (Linux: ${GREEN}server-linux-x64.bin${NOCOLOR}, Mac OS X: ${GREEN}server-macos-x64.bin${NOCOLOR}, Winduz: ${GREEN}server-win-x86.exe${NOCOLOR})"
